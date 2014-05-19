19912f391a5cad864a5e518bf27c2265ccb0f380
James Mortensen <james.mortensen@a-cti.com>
2014-05-18 23:25:35 -0700

    Resolved conflict between PhantomJS, Jasmine, and cloning the console object for testing purposes. Fixed regular expression checks so only native objects or console clones can be overridden.

---
+++ tests/spec/FakeConsoleHelper.js
@@ -0,0 +1,38 @@
+// FakeConsoleHelper.js
+
+
+(function() {
+
+	function FakeConsoleHelper() {};
+
+
+	/**
+	 * Takes the console object as an argument and creates a copy that can be overridden without
+	 * affecting the default console object and its methods.
+	 *
+	 * @param {Object} _console The native console object containing log, info, warn, error, etc. methods.
+	 * @return A cloned object containing the same functionality and properties as console.
+	 */
+	FakeConsoleHelper.makeFakeConsole = function(_console) {
+	  var clonedConsole = {};
+	  for(var k in _console) {
+	  	  if(typeof _console[k] == 'function') {
+	  	  	  //console.debug("override function: " + _console[k]);
+	  	  	  var matchedNativeCodeArray = _console[k].toString().match(/function\s(.*?)\(\)\s{(\s*?)\[native\scode](\s.*?)\}$/);
+	  	  	  var matchedFakeConsoleArray = _console[k].toString().match(/function\s(.*?)\(\)\s{\sconsole\.(.*?)\.apply\(console,\sarguments\);\s\}/);
+	  	  	  if(matchedNativeCodeArray == null && matchedFakeConsoleArray == null) {
+	  	  	      throw new TypeError("Can't clone an object with non-native methods like " + _console[k] + ". The object to clone must contain only native code.");
+	  	  	  }
+	  	  }
+	      if(_console[k].name !== undefined) {
+	          clonedConsole[k] = new Function(
+	             "return function " + _console[k].name + "() { console."+_console[k].name+".apply(console, arguments); }"
+	          )();
+	      }
+	  }
+	  return clonedConsole;
+	};
+
+
+	window.FakeConsoleHelper = FakeConsoleHelper;
+})();
--- tests/spec/SageLogSpec.js
+++ tests/spec/SageLogSpec.js
@@ -1,69 +1,97 @@
 // logsSpec.js
 
 describe("Collecting logs", function() {
+	"use strict";
+
 
     /**
-     * Takes the console object as an argument and creates a copy that can be overridden without
-     * affecting the default console object and its methods.
-     *
-     * @param {Object} _console The native console object containing log, info, warn, error, etc. methods.
-     * @return A cloned object containing the same functionality and properties as console.
+     * PhantomJS doesn't like overridding console in beforeEach and it blocks, so we create a masterFakeConsole 
+     * copy to use instead. The master works just as well as the original. If one attempts to make 
+     * copies of the actual native console object inside the beforeEach and it blocks, Jasmine responds with
+     * "SyntaxError: Parse error (line 1) (1)".
      */
-	function makeFakeConsole(_console) {
-	  var clonedConsole = {};
-	  for(var k in _console) {
-	  	  if(typeof _console[k] == 'function') {
-	  	  	  var matchedNativeCodeArray = _console[k].toString().match(/function\s(.*?)\(\)\s{\s\[native\scode]\s\}$/);
-	  	  	  if(matchedNativeCodeArray == null) {
-	  	  	      throw new TypeError("Can't clone an object with non-native methods. The object to clone must contain only native code.");
-	  	  	  }
-	  	  }
-	      if(_console[k].name !== undefined) {
-	          clonedConsole[k] = new Function(
-	             "return function " + _console[k].name + "(msg){ console."+_console[k].name+".apply(console, arguments);}"
-	          )();
-	      }
-	  }
-	  return clonedConsole;
-	};
+    var masterFakeConsole = FakeConsoleHelper.makeFakeConsole(console);
+    
+    /**
+     * fakeConsole acts as the copy of console used in each test. Prior to being overridden, it functions 
+     * exactly like the default console, but afterwards is decorated by the SageLog library.
+     */
+    var fakeConsole;
+
+    /**
+     * logHandler is reinstantiated with a new SageLog after each test, and it uses a fresh console copy so
+     * every test starts with fresh data and a fresh context.
+     */
+    var logHandler;
 
 
-	var fakeConsole = makeFakeConsole(console);
-	var logHandler = new SageLog(fakeConsole);
-
-	logHandler.init({
-		"captureLogs":true, 
-		"logLevel":SageLog.DEBUG
+    beforeEach(function() {
+	    fakeConsole = FakeConsoleHelper.makeFakeConsole(masterFakeConsole);     // make fake console copy
+	    logHandler = new SageLog(fakeConsole);
 	});
 
-    it("Should set the logging level to INFO and no timestamps", function(done) {
 
+    it("Should set the logging level to INFO and not collect LOG or DEBUG logs", function(done) {
+		
+    	logHandler.init({
+			"captureLogs": true, 
+			"logLevel": 0
+		});
+    	
     	fakeConsole.info('hello world');
-    	fakeConsole.info('hello test');
+    	fakeConsole.error('hello test');
+    	fakeConsole.log('hello nobody');
     	fakeConsole.debug('hello invisible');
+    	fakeConsole.warn('hello careful');
 
     	logHandler.sendLogsToServer();
     	setTimeout(function() {
-	    	///var allLogsAsArray = logHandler.getAllLogsAsArray();
-	    	///fakeConsole.info("all logs as array = " + JSON.stringify(allLogsAsArray));
 	    	var logArray = logHandler.getLogBundleAsArray();
 
 			console.debug("logsCollected = " + logArray);
 			expect(logArray[0].match(/hello world/)).toEqual(['hello world']);
 			expect(logArray[1].match(/hello test/)).toEqual(['hello test']);
-			expect(logArray.length).toEqual(2);
+			expect(logArray[2].match(/hello careful/)).toEqual(['hello careful']);
+			expect(logArray.length).toEqual(3);
 			done();			
 	    },1000);
     });
 
-    it("Should throw an error in the console and also store that error", function() {
+
+    it("should only log errors and nothing else", function(done) {
+   	    
+    	logHandler.init({
+			"captureLogs": true, 
+			"logLevel": SageLog.ERROR
+		});
+
+    	fakeConsole.info('hello world');
+    	fakeConsole.error('hello test');       // only ERROR is logged
+    	fakeConsole.log('hello nobody');
+    	fakeConsole.debug('hello invisible');
+    	fakeConsole.warn('hello careful');
+
+    	setTimeout(function() {
+	    	var logArray = logHandler.getLogBundleAsArray();
+
+			console.debug("logsCollected = " + logArray);
+			expect(logArray[0].match(/hello test/)).toEqual(['hello test']);
+			expect(logArray.length).toEqual(1);
+			done();			
+	    },1000);
+    });
+
+
+    /*it("Should throw an error in the console and also store that error", function() {
     	// TODO: Need to get this working
     	//ttt();
-    	console.log('logs after error = ' + logHandler.getLogBundleAsArray().length);
+    	//console.log('logs after error = ' + logHandler.getLogBundleAsArray().length);
     });
 
     it("should show an error in the logs", function() {
-		console.log('logs after error = ' + logHandler.getLogBundleAsArray().length);
-    });
+		//console.log('logs after error = ' + logHandler.getLogBundleAsArray().length);
+    });*/
+
+
 
 });
